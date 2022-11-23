8b5b20cf9e3be96a0d80cafc14b13643add58a47
nanyp <108845929+nanyp@users.noreply.github.com>
2022-11-23 09:09:34 -0500

    Update app.js

--- back/app.js
+++ back/app.js
@@ -4,7 +4,6 @@ const errorMiddleware = require("./middleware/errors")
 const cookieParser = require("cookie-parser")
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
-const cors = require('cors')
 
 // Using imported constants
 app.use(express.json());
@@ -29,17 +28,5 @@ app.use("/api", ventas);
 
 //MiddleWare to handle errors
 app.use(errorMiddleware)
-app.use(function(req, res, next) {
-      // res.header("Access-Control-Allow-Origin", "*");
-      const allowedOrigins = ['http://localhost:3000', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
-      const origin = req.headers.origin;
-      if (allowedOrigins.includes(origin)) {
-           res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-      res.header("Access-Control-Allow-credentials", true);
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-      next();
-    });
 
 module.exports = app;
